88e4fc052f8240027e6153e5a29bdafd1d4817fd
seokju-na <seokju.me@gmail.com>
2019-03-02 15:08:45 +0900

    필요없는 파일 전부 삭제

--- scripts/copy-icons.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p assets/icons/
-
-cp node_modules/line-awesome/src/production/fa-home.svg assets/icons/home.svg
-cp node_modules/line-awesome/src/production/fa-envelope.svg assets/icons/email.svg
-cp node_modules/line-awesome/src/production/fa-github.svg assets/icons/github.svg
